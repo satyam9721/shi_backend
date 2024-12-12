2dfb9773a655c0fb7c3aeb882c77ba38150d9122
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2024-12-12 16:53:14 +0530

    Delete src directory

--- src/models/Task.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const taskSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-});
-
-module.exports = mongoose.model('Task', taskSchema);
